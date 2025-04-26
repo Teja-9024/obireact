ad7ab8568162a2c546a5ee3367eea945c72b4afa
Teja <teja.mentem@gmail.com>
2025-04-26 18:01:26 +0530

    setuped i18n

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,23 +1,28 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import HttpBackend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
 
-i18n
-  .use(HttpBackend)
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    fallbackLng: 'en',
-    debug: false,
+import * as en from "./en";
+import * as es from "./es";
+import * as fr from "./fr";
 
-    interpolation: {
-      escapeValue: false, // React already escapes
-    },
+const resources = {
+  "en": en,
+  "es": es,
+  "fr": fr
+}
 
-    backend: {
-      loadPath: '/locales/{{lng}}/translation.json',
-    },
-  });
+async function setupI18n() {
+  await i18n
+    .use(initReactI18next)
+    .init({
+      lng: 'en',
+      fallbackLng: 'en',
+      debug: true,
+      resources,
+      interpolation: {
+        escapeValue: false
+      }
+    });
+}
 
-export default i18n;
+setupI18n();  
